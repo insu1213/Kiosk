f49017c2e8f68f0ca52ecccd7970cacaec1e3041
insu1213 <insu12345_@naver.com>
2023-10-23 15:34:31 +0900

    feat: create basictext composable function

---
+++ app/src/main/java/com/insu/kiosk/common/composable/BasicText.kt
@@ -0,0 +1,7 @@
+package com.insu.kiosk.common.composable
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BasicText() {
+}
---
+++ app/src/main/java/com/insu/kiosk/screen/FirstScreen.kt
@@ -0,0 +1,29 @@
+package com.insu.kiosk.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FirstScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            Text(
+                text = "터치해 시작하기"
+            )
+        }
+    }
+}
