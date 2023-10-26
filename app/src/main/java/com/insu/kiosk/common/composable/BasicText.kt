cd41ad6fe04e0d7c2baed556e9de3e206cd00057
insu1213 <insu12345_@naver.com>
2023-10-26 15:11:54 +0900

    feat: Text Composable

--- app/src/main/java/com/insu/kiosk/common/composable/BasicText.kt
+++ app/src/main/java/com/insu/kiosk/common/composable/BasicText.kt
@@ -1,7 +1,35 @@
 package com.insu.kiosk.common.composable
 
+import android.util.TypedValue
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun BasicText() {
+fun BasicText(text: String, fontWeight: FontWeight) {
+    Text(
+        text = text,
+        fontSize = 12.sp,
+        fontWeight = fontWeight,
+        fontStyle = FontStyle.Italic
+    )
+}
+
+@Composable
+fun TitleText(text: String ,fontWeight: FontWeight) {
+    Text(
+        text = text,
+        fontSize = 16.sp,
+        fontWeight = fontWeight,
+        fontStyle = FontStyle.Italic
+    )
 }
